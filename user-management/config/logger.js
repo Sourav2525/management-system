5aa68dd5e3e2cd27e58ba7723f2082a1c7693fa2
Sourav59580 <sourav.1121@zenmonk.tech>
2024-02-13 11:19:41 +0530

    update-error-log

--- user-management/config/logger.js
+++ user-management/config/logger.js
@@ -1,44 +1,20 @@
 /* eslint-disable snakecasejs/snakecasejs */
 const winston = require("winston");
-const morgan = require("morgan");
 
-const { combine, timestamp, json } = winston.format;
+// Define the logger configuration
 const logger = winston.createLogger({
-    level: "http",
-    defaultMeta: {
-        service: "user-management-service",
-    },
-    format: combine(
-        timestamp({
-            format: "YYYY-MM-DD hh:mm:ss.SSS A",
-        }),
-        json()
-    ),
-    transports: [new winston.transports.Console()],
+  level: "error",
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.simple()
+  ),
+  transports: [
+    new winston.transports.Console(), 
+    new winston.transports.File({ filename: "error.log" }) // Log to a file
+  ]
 });
 
-const morgan_middleware = morgan(
-    function (tokens, req, res) {
-        return JSON.stringify({
-            url: tokens.url(req, res),
-            method: tokens.method(req, res),
-            status: Number.parseFloat(tokens.status(req, res)),
-            // content_length: tokens.res(req, res, "content-length"),
-            response_time: Number.parseFloat(tokens["response-time"](req, res)),
-        });
-    },
-    {
-        stream: {
-            // Configure Morgan to use our custom logger with the http severity
-            write: (message) => {
-                const data = JSON.parse(message);
-                logger.http("incoming-request", data);
-            },
-        },
-    }
-);
-
 module.exports = {
-    logger,
-    morgan_middleware
+    logger
 };
--- user-management/routes/index.js
+++ user-management/routes/index.js
@@ -1,18 +1,9 @@
-const { logger } = require("../config/logger");
-
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    try{
-        logger.error("Internal server error");
-        res.json({
-            status: "This is the user management Microservice 1.0.0",
-        });
-    } catch(error) {
-
-        logger.error("Internal server error");
-    }
-   
+    res.json({
+        status: "This is the user management Microservice 1.0.0",
+    });
 });
 
 module.exports = router;
